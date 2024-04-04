096d1ab4e624e92bcd15930965def379428d18d6
slome <slomeec@gmail.com>
2024-04-04 22:03:40 +0500

    Adding early fronend for patch statistic

---
+++ tat-mobile-front/src/components/Content.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <h2>Churn</h2>
+    <v-list v-for="val, item in result.churn">
+      <v-list-item> {{ item }}: {{ Math.round(val) }}%</v-list-item>
+    </v-list>
+  </v-container>
+  <v-container>
+    <h2>Overall</h2>
+    <v-list v-for="val, item in result.overall">
+      <v-list-item> {{ item }}: {{ val }}</v-list-item>
+    </v-list>
+  </v-container>
+</template>
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['getResult']),
+    result() {
+      return this.getResult;
+    }
+  }
+}
+</script>
---
+++ tat-mobile-front/src/pages/mainpage.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-row>
+        <v-col>
+            <v-sheet>
+                <Search />
+            </v-sheet>
+        </v-col>
+        <v-col>
+            <v-sheet>
+                <Content />
+            </v-sheet>
+        </v-col>
+    </v-row>
+</template>
+
+<script lang="ts" setup>
+</script>
