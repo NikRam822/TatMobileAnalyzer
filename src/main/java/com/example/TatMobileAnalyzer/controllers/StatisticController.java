a728c49abdeb2e252328fab6ee165f1367c10fd8
Nikita Ramzin <72182571+NikRam822@users.noreply.github.com>
2024-04-04 13:10:25 +0300

    Features/read patch for period (#29)
    * Configured flexibility for fields in the query and created a Service to add functionality to it
    
    * Updated controller
    
    * Updated controller
    
    * Get statistic of developers with using GitHub Api component
    
    * Add read patches logic for each files
    
    * Added overall and general statistic without churn
    
    * Refactored PatchReader
    
    * Refactoring churn logic
    
    * Add churn metric
    
    * Refactored churn metric
    
    * Update represent view of statistic
    
    * Update author info
    
    * Update return json
    
    * Update refactoring
    
    ---------
    
    Co-authored-by: Timmmich19 <t.akhmatovya@yandex.ru>

--- src/main/java/com/example/TatMobileAnalyzer/controllers/StatisticController.java
+++ src/main/java/com/example/TatMobileAnalyzer/controllers/StatisticController.java
@@ -2,12 +2,18 @@ package com.example.TatMobileAnalyzer.controllers;
 
 import com.example.TatMobileAnalyzer.dto.RepositoryDto;
 import com.example.TatMobileAnalyzer.services.LocFilesService;
+import com.example.TatMobileAnalyzer.services.PatchScanService;
 import com.example.TatMobileAnalyzer.services.StatisticService;
+import com.example.TatMobileAnalyzer.utils.DateUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Map;
 
 @CrossOrigin
 @Slf4j
@@ -17,12 +23,14 @@ public class StatisticController {
 
     private final StatisticService statisticService;
     private final LocFilesService locFilesService;
+    private final PatchScanService patchScanService;
 
 
     @Autowired
-    public StatisticController(StatisticService statisticService, LocFilesService locFilesService) {
+    public StatisticController(StatisticService statisticService, LocFilesService locFilesService, PatchScanService patchScanService) {
         this.statisticService = statisticService;
         this.locFilesService = locFilesService;
+        this.patchScanService = patchScanService;
     }
 
     @GetMapping("/")
@@ -36,13 +44,28 @@ public class StatisticController {
     }
 
     @PostMapping("/loc/statistic")
-    ResponseEntity<String> getStatisticLocFiles(@RequestParam String since,
-                                                @RequestParam String until,
+    ResponseEntity<String> getStatisticLocFiles(@RequestParam(required = false) String since,
+                                                @RequestParam(required = false) String until,
                                                 @RequestBody RepositoryDto repositoryDto) {
-        if (!since.equals("") && !until.equals("")) {
-            since = "?since=" + since;
-            until = "&until=" + until;
+        String period = "";
+        if (since != null || until != null) {
+            period = (since == null) ? "?until=" + until :
+                    (until == null) ? "?since=" + since :
+                            "?since=" + since + "&until=" + until;
         }
-        return locFilesService.getStatisticLocFiles(repositoryDto.getRepositoryUrl(), since, until);
+        return locFilesService.getStatisticLocFiles(repositoryDto.getRepositoryUrl(), period);
+    }
+
+    @PostMapping("/patch/statistic")
+    ResponseEntity<Map<String, Object>> getStatisticPatchScan(@RequestParam(required = false) String since,
+                                                              @RequestParam(required = false) String until,
+                                                              @RequestBody RepositoryDto repositoryDto) throws ParseException {
+
+
+        Date startDate = DateUtils.parseDate(since, "yyyy-MM-dd");
+        Date endDate = DateUtils.parseDate(until, "yyyy-MM-dd");
+
+
+        return patchScanService.getStatisticPatchScan(repositoryDto.getRepositoryUrl(), startDate, endDate);
     }
 }
