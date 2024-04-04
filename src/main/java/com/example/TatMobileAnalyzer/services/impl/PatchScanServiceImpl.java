4baf8ca6e994ba6678315cc831a8cf335e9cd164
Nikita Ramzin <rivorn73@gmail.com>
2024-04-04 13:04:34 +0300

    Update refactoring

--- src/main/java/com/example/TatMobileAnalyzer/services/impl/PatchScanServiceImpl.java
+++ src/main/java/com/example/TatMobileAnalyzer/services/impl/PatchScanServiceImpl.java
@@ -42,39 +42,26 @@ public class PatchScanServiceImpl implements PatchScanService {
         Map<String, Map<String, Integer>> generalResult = new HashMap<>();
         Map<String, Double> churn = new HashMap<>();
 
+        processCommits(commitsPerPeriod, authorStats, overall, general, generalResult, churn);
+
+        Map<String, Object> finalStats = new HashMap<>();
+        finalStats.put("authorStats", authorStats);
+        finalStats.put("overall", overall);
+        finalStats.put("general", generalResult);
+        finalStats.put("churn", churn);
+
+        return ResponseEntity.ok(finalStats);
+    }
+
+    @SneakyThrows
+    private void processCommits(List<GHCommit> commitsPerPeriod, Map<String, List<Map<String, Object>>> authorStats,
+                                Map<String, Integer> overall, Map<String, Map<Integer, String>> general,
+                                Map<String, Map<String, Integer>> generalResult, Map<String, Double> churn) {
         for (GHCommit commit : commitsPerPeriod) {
             String author = commit.getCommitShortInfo().getAuthor().getName();
             List<Map<String, Object>> authorCommits = authorStats.getOrDefault(author, new ArrayList<>());
 
-            List<GHCommit.File> files = commit.listFiles().toList();
-            List<Map<String, Object>> fileStats = new ArrayList<>();
-
-            for (GHCommit.File file : files) {
-                String patch = file.getPatch();
-                PatchReader.PatchInfo patchInfo = PatchReader.readPatch(patch);
-
-                Map<String, Object> fileStat = new HashMap<>();
-                fileStat.put("filename", file.getFileName());
-                fileStat.put("add all", file.getLinesAdded());
-                fileStat.put("del all", file.getLinesDeleted());
-                //fileStat.put("path", file.getPatch());
-
-                // fileStat.put("add", patchInfo.getAdd());
-                // fileStat.put("del", patchInfo.getDel());
-
-                fileStats.add(fileStat);
-
-                // Обновление статистики overall
-                overall.put(author, overall.getOrDefault(author, 0) + patchInfo.getAdd().size());
-
-                // Обновление статистики general
-                Map<Integer, String> fileLines = general.getOrDefault(file.getFileName(), new HashMap<>());
-                for (String addedLine : patchInfo.getAdd()) {
-                    int lineNumber = Integer.parseInt(addedLine.split(". ")[0]);
-                    fileLines.put(lineNumber, author);
-                }
-                general.put(file.getFileName(), fileLines);
-            }
+            List<Map<String, Object>> fileStats = processCommitFiles(commit, general, overall, author);
 
             Map<String, Object> commitStat = new HashMap<>();
             commitStat.put("sha", commit.getSHA1());
@@ -88,7 +75,51 @@ public class PatchScanServiceImpl implements PatchScanService {
             authorStats.put(author, authorCommits);
         }
 
-        // Расчет статистики churn
+        calculateChurn(authorStats, overall, general, generalResult, churn);
+    }
+
+    @SneakyThrows
+    private List<Map<String, Object>> processCommitFiles(GHCommit commit, Map<String, Map<Integer, String>> general,
+                                                         Map<String, Integer> overall, String author) {
+        List<Map<String, Object>> fileStats = new ArrayList<>();
+
+        List<GHCommit.File> files = commit.listFiles().toList();
+        for (GHCommit.File file : files) {
+            String patch = file.getPatch();
+            PatchReader.PatchInfo patchInfo = PatchReader.readPatch(patch);
+
+            Map<String, Object> fileStat = new HashMap<>();
+            fileStat.put("filename", file.getFileName());
+            fileStat.put("add all", file.getLinesAdded());
+            fileStat.put("del all", file.getLinesDeleted());
+            //fileStat.put("path", file.getPatch());
+
+            // fileStat.put("add", patchInfo.getAdd());
+            // fileStat.put("del", patchInfo.getDel());
+
+            fileStats.add(fileStat);
+
+            overall.put(author, overall.getOrDefault(author, 0) + patchInfo.getAdd().size());
+
+            updateGeneralStatistics(file, patchInfo, general, author);
+        }
+        return fileStats;
+    }
+
+    @SneakyThrows
+    private void updateGeneralStatistics(GHCommit.File file, PatchReader.PatchInfo patchInfo, Map<String, Map<Integer, String>> general, String author) {
+        Map<Integer, String> fileLines = general.getOrDefault(file.getFileName(), new HashMap<>());
+        for (String addedLine : patchInfo.getAdd()) {
+            int lineNumber = Integer.parseInt(addedLine.split(". ")[0]);
+            fileLines.put(lineNumber, author);
+        }
+        general.put(file.getFileName(), fileLines);
+    }
+
+
+    private void calculateChurn(Map<String, List<Map<String, Object>>> authorStats, Map<String, Integer> overall,
+                                Map<String, Map<Integer, String>> general, Map<String, Map<String, Integer>> generalResult,
+                                Map<String, Double> churn) {
         for (Map.Entry<String, List<Map<String, Object>>> entry : authorStats.entrySet()) {
             String author = entry.getKey();
             Integer totalLines = overall.get(author);
@@ -101,7 +132,6 @@ public class PatchScanServiceImpl implements PatchScanService {
                 }
             }));
 
-
             for (String fileName : general.keySet()) {
                 Map<String, Integer> generalStats = new HashMap<>();
                 general.get(fileName).forEach((lineNumber, authorOfAdd) -> {
@@ -112,18 +142,8 @@ public class PatchScanServiceImpl implements PatchScanService {
             if (totalLines != null && totalLines != 0) {
                 churn.put(author, 100 - (((double) ref.addLines / totalLines) * 100));
             } else {
-
                 churn.put(author, 0.0);
             }
         }
-
-
-        Map<String, Object> finalStats = new HashMap<>();
-        finalStats.put("authorStats", authorStats);
-        finalStats.put("overall", overall);
-        finalStats.put("general", generalResult);
-        finalStats.put("churn", churn);
-
-        return ResponseEntity.ok(finalStats);
     }
 }
