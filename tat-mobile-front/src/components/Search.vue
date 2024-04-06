cd3b62d0f63048cd6487dca6bd60dc3ed0dde7a0
slome <slomeec@gmail.com>
2024-04-07 03:36:15 +0500

    make statistic in a table

--- tat-mobile-front/src/components/Search.vue
+++ tat-mobile-front/src/components/Search.vue
@@ -1,24 +1,29 @@
 <template>
-    <v-form @submit.prevent>
-        <v-row>
-            <v-col>
-                <v-container>
-                    <v-text-field :rules="rules" v-model="repo" label="Enter repository URL"></v-text-field>
-                </v-container>
-            </v-col>
-            <v-col>
-                <v-container>
-                    <v-text-field v-model="start" label="Start date"></v-text-field>
-                    <v-text-field v-model="end" label="End date"></v-text-field>
-                </v-container>
-            </v-col>
-        </v-row>
-        <v-btn size="x-large" @click="getResult" type="submit"> Search </v-btn>
-    </v-form>
+    <v-sheet>
+        <v-form @submit.prevent>
+            <v-row>
+                <v-col>
+                    <v-container>
+                        <v-text-field focused="true" variant="outlined" :rules="rules" v-model="repo"
+                            label="Enter repository URL"></v-text-field>
+                    </v-container>
+                </v-col>
+                <v-col>
+                    <v-container>
+                        <v-text-field style="width: 200px" type="date" variant="outlined" v-model="start"
+                            label="Start date"></v-text-field>
+                        <v-text-field style="width: 200px" type="date" variant="outlined" v-model="end"
+                            label="End date"></v-text-field>
+                    </v-container>
+                </v-col>
+            </v-row>
+            <v-btn size="x-large" @click="getResult" type="submit"> Get result </v-btn>
+        </v-form>
+    </v-sheet>
 </template>
 <script>
-    export default {
-        data: () => ({
+export default {
+    data: () => ({
         repo: "",
         start: "",
         end: "",
@@ -35,5 +40,5 @@
             this.$emit('get-result', this.repo, this.start, this.end)
         }
     }
-    }
+}
 </script>
