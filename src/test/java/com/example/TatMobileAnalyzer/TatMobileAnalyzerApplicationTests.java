23f2459cb4f31ec8dd7052524823a3af05d7f677
Amir <amirabikhuja@gmail.com>
2024-04-14 18:39:20 +0300

    Create Dockerfile for the application #40
    * Create Dockerfile for the application
    
    * Add docker build instruction to README.md

--- src/test/java/com/example/TatMobileAnalyzer/TatMobileAnalyzerApplicationTests.java
+++ src/test/java/com/example/TatMobileAnalyzer/TatMobileAnalyzerApplicationTests.java
@@ -1,13 +1,6 @@
 package com.example.TatMobileAnalyzer;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TatMobileAnalyzerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+class TatMobileAnalyzerApplicationTests {}
