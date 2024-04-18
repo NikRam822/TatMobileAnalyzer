0b3680d07dd28f09a64daea955d3803cc05e8d5f
slome <slomeec@gmail.com>
2024-04-19 01:29:51 +0500

    Added simple visual on a mainpage

--- tat-mobile-front/src/pages/mainpage.vue
+++ tat-mobile-front/src/pages/mainpage.vue
@@ -1,60 +1,90 @@
 <template>
     <v-layout>
-      <v-app-bar>
-        <v-app-bar-nav-icon color="lime-accent-3">
-        </v-app-bar-nav-icon>
-        <v-app-bar-title class="text-lime-accent-3">Dashboard</v-app-bar-title>
-      </v-app-bar>
-      <v-main>
-        <div class="ma-5">
-          <v-row>
-              <v-col>
+        <!-- Нужно закрепить app-bar -->
+        <v-app-bar>
+            <!-- Нужно переделать, пока что обошел с max-width -->
+            <v-img src="@/assets/logo.svg" height="40" max-width="64"></v-img>
+            <v-app-bar-title class="text-lime-accent-3">Dashboard</v-app-bar-title>
+            <v-spacer></v-spacer>
+            <v-icon icon="mdi-account" size="40px" class="mr-10"></v-icon>
+        </v-app-bar>
+        <v-main>
+            <!-- Пока что использую v-sheet, хоть и не по назначению, за то удобно -->
+            <v-sheet class="pa-6 ma-4" rounded="xl" elevation="4">
+                <v-container fluid="true">
+                    <v-row>
+                        <v-col v-for="rep in repositories" cols="auto">
+                            <v-card rounded="xl" height="200" width="400" border="md">
+                                <v-card-subtitle>{{ rep }}</v-card-subtitle>
+                            </v-card>
+                        </v-col>
+                        <v-col>
+                            <v-card @click="cardAppend = !cardAppend" v-if="cardAppend" rounded="xl" height="200"
+                                width="400" border="md">
+                                <!-- Надо поставить плюс в середину-->
+                                <v-col align="center">
+                                    <v-icon icon="mdi-plus-circle-outline" size="150" color="rgb(92, 99, 106)"></v-icon>
+                                </v-col>
+                            </v-card>
+                            <v-card v-else rounded="xl" height="200" width="400" border="md">
+                                <v-form @submit.prevent="repositories.push(rep)">
+                                    <v-text-field v-model="rep" label="Enter reposytory URL" type="url"></v-text-field>
+                                    <v-col align="center">
+                                        <v-btn :disabled="!rep" type="submit" icon="mdi-plus-circle-outline"></v-btn>
+                                    </v-col>
+                                </v-form>
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-sheet>
+            <!-- <v-row>
+                <v-col>
                     <Search @get-result="getRepos" />
-              </v-col>
-              <v-col>
+                </v-col>
+                <v-col>
                     <Content :result="result" :loader="loader" :showContent="showContent" />
-              </v-col>
-          </v-row>
-        </div>
-      </v-main>
+                </v-col>
+            </v-row> -->
+        </v-main>
     </v-layout>
 </template>
 
 <script>
 import axios from 'axios'
-import dotenv from 'dotenv'
-
-dotenv.config()
 export default {
     data: () => ({
-        result: [],
-        loader: false,
-        showContent: false
+        cardAppend: true,
+        rep: '',
+        repositories: [],
+        // result: [],
+        // loader: false,
+        // showContent: false
     }),
     methods: {
-        async getRepos(repo, start, end) {
-            let hostadress = process.env.VUE_APP_HOST_ADDRESS;
-            this.loader = true
-            this.showContent = false
-            if (start && end) {
-                hostadress += "?since=" + start + "&until=" + end
-            } else if (start) {
-                hostadress += "?since=" + start
-            } else if (end) {
-                hostadress += "?until=" + end
-            }
+        // async getRepos(repo, start, end) {
+        //     let hostadress = "http://localhost:8080/patch/statistic"
+        //     this.loader = true
+        //     this.showContent = false
+        //     if (start && end) {
+        //         hostadress += "?since=" + start + "&until=" + end
+        //     } else if (start) {
+        //         hostadress += "?since=" + start
+        //     } else if (end) {
+        //         hostadress += "?until=" + end
+        //     }
 
-            try {
-                const response = await axios.post(hostadress, {
-                    repositoryUrl: repo,
-                });
-                this.result = response.data;
-            } catch (error) {
-                console.error('Error fetching repositories:', error);
-            }
-            this.loader = false
-            this.showContent = true
-        }
+        //     try {
+        //         const response = await axios.post(hostadress, {
+        //             repositoryUrl: repo,
+        //         });
+        //         this.result = response.data;
+        //     } catch (error) {
+        //         console.error('Error fetching repositories:', error);
+        //     }
+        //     this.loader = false
+        //     this.showContent = true
+        // }
     }
 }
 </script>
