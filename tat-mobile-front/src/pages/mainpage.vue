9459006471d1944dd9b63346db986106662adf72
slome <slomeec@gmail.com>
2024-04-19 11:29:28 +0500

    fixed the progress bar

--- tat-mobile-front/src/pages/mainpage.vue
+++ tat-mobile-front/src/pages/mainpage.vue
@@ -1,17 +1,14 @@
 <template>
   <v-layout>
-    <!-- Нужно закрепить app-bar -->
     <v-app-bar>
-      <!-- Нужно переделать, пока что обошел с max-width -->
       <v-img src="../assets/Logo.svg" height="40" max-width="64"></v-img>
       <v-app-bar-title class="text-lime-accent-3">Dashboard</v-app-bar-title>
       <v-spacer></v-spacer>
       <v-icon icon="mdi-account" size="40px" class="mr-10"></v-icon>
     </v-app-bar>
     <v-main>
-      <!-- Пока что использую v-sheet, хоть и не по назначению, за то удобно -->
       <v-sheet class="pa-6 ma-4" rounded="xl" elevation="4">
-        <v-container fluid="true">
+        <v-container>
           <v-row>
             <v-col v-for="val, url in this.$store.state.repositories" cols="auto">
               <v-card @click="navigateToOtherPage(url)" rounded="xl" height="150" width="400" border="md">
@@ -19,7 +16,8 @@
                   <v-row>
                     {{ url.slice(url.lastIndexOf('/') + 1) }}
                     <v-spacer />
-                    <v-icon @click="this.$store.commit('delRepos', url)" icon="mdi-trash-can-outline" />
+                    <v-btn flat @click.stop="this.$store.commit('delRepos', url)" icon="mdi-trash-can-outline">
+                    </v-btn>
                   </v-row>
                 </v-card-title>
                 <v-card-subtitle class="ma-3">{{ url }}</v-card-subtitle>
@@ -29,7 +27,6 @@
 
               <v-card @click="cardAppend = !cardAppend" v-if="cardAppend" rounded="xl" height="150" width="400"
                 border="md">
-                <!-- Надо поставить плюс в середину-->
                 <v-col align="center">
                   <v-icon icon="mdi-plus-circle-outline" size="150" color="rgb(92, 99, 106)"></v-icon>
                 </v-col>
@@ -39,10 +36,14 @@
                 <v-form @submit.prevent="addCard()">
                   <v-text-field required :rules="[re.test(rep) || 'Wrong URL']" v-model="rep"
                     label="Enter reposytory URL" type="url"></v-text-field>
-                  <v-progress-linear v-show="loader" color="rgb(92, 99, 106)" height="6" indeterminate
-                    rounded></v-progress-linear>
-                  <v-col align="center">
-                    <v-btn :disabled="!rep" type="submit" icon="mdi-plus-circle-outline"></v-btn>
+                  <v-container v-show="loader" class="mt-2">
+                    <v-row justify="center">
+                      <v-progress-linear color="rgb(92, 99, 106)" height="6" indeterminate rounded></v-progress-linear>
+                      <p> Analyzing reposytory </p>
+                    </v-row>
+                  </v-container>
+                  <v-col v-show="!loader" align="center">
+                    <v-btn elevation="2" :disabled="!rep" type="submit" icon="mdi-plus-circle-outline"></v-btn>
                   </v-col>
                 </v-form>
               </v-card>
@@ -66,16 +67,12 @@ export default {
     loader: false,
   }),
   methods: {
-    addCard() {
+    async addCard() {
       this.loader = true
       if (this.re.test(this.rep)) {
-        this.getRepos()
       } else {
         alert('URl should be: https://github.com/AUTOR/REPO')
       }
-      this.loader = false
-    },
-    async getRepos() {
       let hostadress = "http://localhost:8080/patch/statistic"
       try {
         const response = await axios.post(hostadress, {
@@ -86,6 +83,11 @@ export default {
       } catch (error) {
         console.error('Error fetching repositories:', error);
       }
+      this.rep = ''
+      this.loader = false
+    },
+    navigateToOtherPage(repoName) {
+      this.$router.push({ path: '/project-review', query: { repoName: repoName } });
     }
   }
 }
