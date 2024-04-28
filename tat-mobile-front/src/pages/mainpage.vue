5dd472ce54e471acca34c79a867de4d98322c768
slome <slomeec@gmail.com>
2024-04-28 21:38:20 +0500

    Cards changes

--- tat-mobile-front/src/pages/mainpage.vue
+++ tat-mobile-front/src/pages/mainpage.vue
@@ -2,7 +2,7 @@
   <v-layout>
     <v-app-bar>
       <v-img src="../assets/Logo.svg" height="40" max-width="64"></v-img>
-      <v-app-bar-title class="text-lime-accent-3">Dashboard</v-app-bar-title>
+      <v-app-bar-title style="color: rgb(197, 226, 21)">Dashboard</v-app-bar-title>
       <v-spacer></v-spacer>
       <v-icon icon="mdi-account" size="40px" class="mr-10"></v-icon>
     </v-app-bar>
