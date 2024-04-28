8804a10ad9a8bac5d1e8b5cd6ca6bae78353e10f
slome <slomeec@gmail.com>
2024-04-28 14:08:36 +0500

    Some changes

---
+++ tat-mobile-front/src/components/CardsFolder.vue
@@ -0,0 +1,84 @@
+<template>
+    <v-sheet height="89dvh" class="pa-4 ma-5" rounded="xl" elevation="4">
+        <v-container fluid>
+            <v-row>
+                <v-col v-for="val, url in this.$store.state.repositories" cols="auto">
+                    <v-card @click="navigateToOtherPage(url)" rounded="xl" height="150" width="400" border="md">
+                        <v-card-title class="ma-5">
+                            <v-row>
+                                {{ url.slice(url.lastIndexOf('/') + 1) }}
+                                <v-spacer />
+                                <v-btn flat @click.stop="this.$store.commit('delRepos', url)"
+                                    icon="mdi-trash-can-outline">
+                                </v-btn>
+                            </v-row>
+                        </v-card-title>
+                        <v-card-subtitle class="ma-3">{{ url }}</v-card-subtitle>
+                    </v-card>
+                </v-col>
+                <v-col cols="auto">
+
+                    <v-card @click="cardAppend = !cardAppend" v-if="cardAppend" rounded="xl" height="150" width="400"
+                        border="md" class="d-flex justify-center">
+                        <v-icon icon="mdi-plus-circle-outline" size="130" color="rgb(92, 99, 106)"
+                            class="align-self-center"></v-icon>
+                    </v-card>
+
+                    <v-card v-else rounded="xl" height="150" width="400" border="md">
+                        <v-form @submit.prevent="addCard()" class="d-flex flex-column">
+                            <v-text-field required :rules="[re.test(rep) || 'Wrong URL']" v-model="rep"
+                                label="Enter reposytory URL" type="url"></v-text-field>
+                            <v-container v-show="loader" class="mt-2">
+                                <v-row justify="center">
+                                    <v-progress-linear color="rgb(92, 99, 106)" height="6" indeterminate
+                                        rounded></v-progress-linear>
+                                    <p> Analyzing reposytory </p>
+                                </v-row>
+                            </v-container>
+                            <v-btn v-show="!loader" elevation="2" :disabled="!rep" type="submit"
+                                icon="mdi-plus-circle-outline" class="align-self-center"></v-btn>
+                        </v-form>
+                    </v-card>
+
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-sheet>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    data: () => ({
+        re: new RegExp("^https://github.com/([^/]+)/([^/]+)$"),
+        cardAppend: true,
+        rep: '',
+        result: [],
+        loader: false,
+    }),
+    methods: {
+        async addCard() {
+            this.loader = true
+            if (this.re.test(this.rep)) {
+            } else {
+                alert('URl should be: https://github.com/AUTOR/REPO')
+            }
+            let hostadress = "http://localhost:8080/patch/statistic"
+            try {
+                const response = await axios.post(hostadress, {
+                    repositoryUrl: this.rep,
+                });
+                this.result = response.data
+                this.$store.commit('addRepos', [this.rep, this.result])
+            } catch (error) {
+                console.error('Error fetching repositories:', error);
+            }
+            this.rep = ''
+            this.cardAppend = true
+            this.loader = false
+        },
+        navigateToOtherPage(repoName) {
+            this.$router.push({ path: '/project-review', query: { repoName: repoName } });
+        }
+    }
+}
+</script>
--- tat-mobile-front/src/pages/project-review.vue
+++ tat-mobile-front/src/pages/project-review.vue
@@ -1,33 +1,14 @@
 <template>
   <v-layout>
-    <!-- Нужно закрепить app-bar -->
     <v-app-bar>
-      <!-- Нужно переделать, пока что обошел с max-width -->
       <v-img src="../assets/Logo.svg" height="40" max-width="64"></v-img>
       <v-app-bar-title class="text-lime-accent-3">{{ repos.slice(repos.lastIndexOf('/') + 1) }}</v-app-bar-title>
       <v-spacer></v-spacer>
       <v-icon icon="mdi-account" size="40px" class="mr-10"></v-icon>
     </v-app-bar>
     <v-main>
-      <v-sheet class="pa-6 ma-4" rounded="xl" elevation="4">
-        <v-table density="compact">
-          <thead>
-            <tr>
-              <th> Name </th>
-              <th> Overall </th>
-              <th> Churn </th>
-              <th> Total commits </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="churn, name in this.$store.state.repositories[repos].churn">
-              <td> {{ name }} </td>
-              <td> {{ this.$store.state.repositories[repos].overall[name] }}</td>
-              <td> {{ Math.round(churn) }} </td>
-              <td> {{ Math.round(this.$store.state.repositories[repos].overall[name] * ((100 - churn) / 100)) }} </td>
-            </tr>
-          </tbody>
-        </v-table>
+      <v-sheet class="pa-4 ma-4" height="89dvh" rounded="xl" elevation="4">
+        <Content />
       </v-sheet>
     </v-main>
   </v-layout>
@@ -36,8 +17,6 @@
 <script>
 export default {
   data: () => ({
-    repos: [],
-    statistic: []
   }),
   created() {
     this.repos = this.$route.query.repoName;
