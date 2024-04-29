b78897a08789ae7c1da56d8e6a671140dddf835b
Nikita Ramzin <rivorn73@gmail.com>
2024-04-29 13:53:50 +0300

    Add config for cors config

--- src/main/java/com/example/TatMobileAnalyzer/config/CorsConfig.java
+++ src/main/java/com/example/TatMobileAnalyzer/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.TatMobileAnalyzer.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,10 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.host}")
+    private String host;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000/")
+                .allowedOrigins(host)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Accept",
                         "Content-Length",
