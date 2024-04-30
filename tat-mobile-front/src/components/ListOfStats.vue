2d9cc122a22d272ecb3e4ada2e22cb99d6dd8249
slome <slomeec@gmail.com>
2024-04-30 19:28:47 +0500

    Update table

--- tat-mobile-front/src/components/ListOfStats.vue
+++ tat-mobile-front/src/components/ListOfStats.vue
@@ -1,35 +1,16 @@
 <template>
     <v-card>
         <v-list>
-            <v-list-item v-for="(val) in statistics"><v-btn @click='executeFunction(val)'> {{ val
-                    }} </v-btn></v-list-item>
+            <v-list-item v-for="(val) in statistics" @click='executeFunction(val)'> {{ val
+                }} <v-divider></v-divider></v-list-item>
         </v-list>
     </v-card>
 
-    <v-table v-if="showTable">
-        <thead>
-            <tr>
-                <th> Name </th>
-                <th> Overall </th>
-                <th> Churn </th>
-                <th> Total </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="autor in this.$store.state.stats">
-                <td><v-btn>{{ autor.name }}</v-btn></td>
-                <td>{{ autor.overall }}</td>
-                <td>{{ autor.churn }}</td>
-                <td>{{ autor.total }}</td>
-            </tr>
-        </tbody>
-    </v-table>
 </template>
 <script>
 export default {
     data: () => ({
         currentStat: '',
-        showTable: false,
         // Это список содержащий различные статистики (Пока что одна только раьботает)
         statistics: ["Churn Statistics", "BlaBLALFA", "Statistica"]
     }),
@@ -56,7 +37,6 @@ export default {
                 stats[stats.length - 1].overall = statsRepo.overall[key]
                 stats[stats.length - 1].total = Math.round(stats[stats.length - 1].overall * stats[stats.length - 1].churn / 100)
             }
-            this.showTable = true
             this.$store.commit("pushStats", stats)
         }
     }
