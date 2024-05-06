388341705f6611981aa071846d0dff3271cbfc35
slome <slomeec@gmail.com>
2024-05-06 20:48:03 +0500

    fixed linear cirular

--- tat-mobile-front/src/components/cards/CardOfProject.vue
+++ tat-mobile-front/src/components/cards/CardOfProject.vue
@@ -11,12 +11,6 @@
       class="d-flex flex-column"
       style="max-width: 500px; min-width: 200px"
     >
-      <v-btn
-        @click.stop="deleteProject"
-        flat
-        icon="mdi-trash-can-outline"
-        class="align-self-end ma-4"
-      ></v-btn>
       <template v-slot:append>
         <v-btn
           @click.stop="updateFavor(rep.favorite, rep.projectId)"
@@ -25,14 +19,10 @@
         ></v-btn>
       </template>
       <v-container v-show="loader">
-        <v-progress-linear
-          color="rgb(92, 99, 106)"
-          height="6"
-          indeterminate
-          rounded
-        ></v-progress-linear>
+        <v-progress-linear color="rgb(92, 99, 106)" height="6" indeterminate rounded></v-progress-linear>
         <p>Analyzing reposytory</p>
       </v-container>
+      <v-btn @click.stop="deleteProject" flat icon="mdi-trash-can-outline" class="align-self-end ma-4"></v-btn>
     </v-card>
   </v-col>
 </template>
