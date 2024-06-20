0d585441dc23bd600036a36ee2426fe829a09eac
Nikita Ramzin <72182571+NikRam822@users.noreply.github.com>
2024-06-21 00:30:34 +0300

    Add cocomo functionality (#73)

---
+++ src/main/java/com/example/TatMobileAnalyzer/dto/CocomoCalculateDto.java
@@ -0,0 +1,12 @@
+package com.example.TatMobileAnalyzer.dto;
+
+import lombok.Data;
+
+@Data
+public class CocomoCalculateDto {
+
+    double personMonths;
+    double months;
+    double personnel;
+    double laborIntensityWithRisk;
+}
---
+++ src/main/java/com/example/TatMobileAnalyzer/services/impl/cocomo/CocomoServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.TatMobileAnalyzer.services.impl.cocomo;
+
+import com.example.TatMobileAnalyzer.dto.CocomoCalculateDto;
+import com.example.TatMobileAnalyzer.dto.CocomoDto;
+import com.example.TatMobileAnalyzer.services.CocomoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CocomoServiceImpl implements CocomoService {
+    @Override
+    public CocomoCalculateDto calculateCocomoMetrics(CocomoDto cocomoDto) {
+
+        CocomoCoefficients cocomoCoefficients = new CocomoCoefficients(cocomoDto.getProjectType());
+        CocomoCalculateDto cocomoCalculateDto = new CocomoCalculateDto();
+        cocomoCalculateDto.setPersonMonths((double) Math.round(cocomoCoefficients.a() * Math.pow(cocomoDto.getKloc(), cocomoCoefficients.b()) * 100) / 100);
+        cocomoCalculateDto.setMonths((double) Math.round(cocomoCoefficients.c() * Math.pow(cocomoCalculateDto.getPersonMonths(), cocomoCoefficients.d()) * 100) / 100);
+        cocomoCalculateDto.setPersonnel((double) Math.round(cocomoCalculateDto.getPersonMonths() / cocomoCalculateDto.getMonths() * 100) / 100);
+
+        cocomoCalculateDto.setLaborIntensityWithRisk(
+                Math.round(cocomoCalculateDto.getPersonMonths() *
+                        cocomoDto.getReliability() *
+                        cocomoDto.getDatabaseSize() *
+                        cocomoDto.getProductComplexity() *
+                        cocomoDto.getPerformance() *
+                        cocomoDto.getMemoryLimit() *
+                        cocomoDto.getUnstableEnvironment() *
+                        cocomoDto.getRecoveryTime() *
+                        cocomoDto.getAnalyticalSkills() *
+                        cocomoDto.getDevelopmentSkills() *
+                        cocomoDto.getDevelopmentExperience() *
+                        cocomoDto.getVirtualMachinesExperience() *
+                        cocomoDto.getLanguageExperience() *
+                        cocomoDto.getDevelopmentTools() *
+                        cocomoDto.getDevelopmentMethods() *
+                        cocomoDto.getDevelopmentSchedule() * cocomoCoefficients.risk() * 100.0) / 100.0
+        );
+
+        return cocomoCalculateDto;
+    }
+}
+
