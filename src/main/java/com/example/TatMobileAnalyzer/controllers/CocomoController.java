dc4e980b57d4fad36e113a0159c7eb2683568e66
Nikita Ramzin <rivorn73@gmail.com>
2024-06-21 14:14:11 +0300

    Update cors config

--- src/main/java/com/example/TatMobileAnalyzer/controllers/CocomoController.java
+++ src/main/java/com/example/TatMobileAnalyzer/controllers/CocomoController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin
+
 @Slf4j
 @RestController
 @RequestMapping("api/cocomo")
