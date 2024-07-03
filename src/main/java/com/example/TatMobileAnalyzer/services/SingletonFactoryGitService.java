53d390fda902a61ade0b785fbd0cc9671c09a76e
Nikita Ramzin <72182571+NikRam822@users.noreply.github.com>
2024-07-03 04:51:58 +0300

    Update SingletonFactoryGitService

--- src/main/java/com/example/TatMobileAnalyzer/services/SingletonFactoryGitService.java
+++ src/main/java/com/example/TatMobileAnalyzer/services/SingletonFactoryGitService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @Slf4j
@@ -14,6 +16,8 @@ public class SingletonFactoryGitService {
 
     private final GitHubService gitHubService;
     private final GitLabService gitLabService;
+
+    private final Map<String, GitService> serviceMap = new ConcurrentHashMap<>();
 
     private static SingletonFactoryGitService instance;
 
@@ -25,6 +29,8 @@ public class SingletonFactoryGitService {
 
     @PostConstruct
     public void init() {
+        serviceMap.put("github", (GitService) gitHubService);
+        serviceMap.put("gitlab", (GitService) gitLabService);
         instance = this;
     }
 
@@ -33,23 +39,19 @@ public class SingletonFactoryGitService {
         return instance;
     }
 
-
     public GitService getImplementation(String repositoryUrl) {
-
         if (repositoryUrl == null || repositoryUrl.isEmpty()) {
             log.warn("Repository URL is not supported: {}", repositoryUrl);
             return null;
         }
 
-        if (repositoryUrl.contains("github")) {
-            return (GitService) gitHubService;
-        } else if (repositoryUrl.contains("gitlab")) {
-            return (GitService) gitLabService;
-        } else {
-            log.warn("Repository URL is not supported: {}", repositoryUrl);
-            return null;
-        }
+        return serviceMap.entrySet().stream()
+                .filter(entry -> repositoryUrl.contains(entry.getKey()))
+                .map(Map.Entry::getValue)
+                .findFirst()
+                .orElseGet(() -> {
+                    log.warn("Repository URL is not supported: {}", repositoryUrl);
+                    return null;
+                });
     }
-
-
 }
