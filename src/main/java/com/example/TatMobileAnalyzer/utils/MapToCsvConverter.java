d41a92557db69c4fc4fdceb9a4c8abcbaf1341d7
Nikita Ramzin <72182571+NikRam822@users.noreply.github.com>
2024-07-12 02:13:34 +0300

    Refactor MapToCsv (#92)

--- src/main/java/com/example/TatMobileAnalyzer/utils/MapToCsvConverter.java
+++ src/main/java/com/example/TatMobileAnalyzer/utils/MapToCsvConverter.java
@@ -1,27 +1,27 @@
 package com.example.TatMobileAnalyzer.utils;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-
+@Service
+@Slf4j
 public class MapToCsvConverter {
 
     public static void mapToCsv(Map<String, List<Map<String, Object>>> data, String outputPath) {
         try (FileWriter writer = new FileWriter(outputPath)) {
-            // Заголовок CSV файла
             writer.write("Filename,Author,Additions,Deletions,Changes,CommitSHA,CommitDate\n");
 
-            // Проходим по всем файлам
             for (Map.Entry<String, List<Map<String, Object>>> fileEntry : data.entrySet()) {
                 String filename = fileEntry.getKey();
 
-                // Проходим по всем записям для данного файла
                 for (Map<String, Object> contributorEntry : fileEntry.getValue()) {
                     String author = contributorEntry.keySet().iterator().next();
                     List<Map<String, Object>> statsList = (List<Map<String, Object>>) contributorEntry.get(author);
 
-                    // Проходим по статистике для данного автора
                     for (Map<String, Object> stats : statsList) {
                         writer.write(filename + ",");
                         writer.write(author + ",");
@@ -34,7 +34,7 @@ public class MapToCsvConverter {
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("Error writing to file {}", outputPath, e);
         }
     }
 }
