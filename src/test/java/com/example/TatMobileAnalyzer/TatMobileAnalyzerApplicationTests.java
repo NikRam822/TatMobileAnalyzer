088ac6215f57cc04084c313e625513ad01904c6d
Nikita Ramzin <72182571+NikRam822@users.noreply.github.com>
2024-07-18 12:45:41 +0300

    Add tests (#96)
    * Add tests
    
    * Add tests

--- src/test/java/com/example/TatMobileAnalyzer/TatMobileAnalyzerApplicationTests.java
+++ src/test/java/com/example/TatMobileAnalyzer/TatMobileAnalyzerApplicationTests.java
@@ -1,6 +1,26 @@
 package com.example.TatMobileAnalyzer;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-class TatMobileAnalyzerApplicationTests {}
+class TatMobileAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+    @Test
+    void mainMethodLoadsContext() {
+        String[] args = {};
+        ConfigurableApplicationContext context = new SpringApplicationBuilder(TatMobileAnalyzerApplication.class)
+                .run(args);
+        assertNotNull(context);
+        context.close();
+    }
+}
